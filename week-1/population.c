748d8571577fb55dc2d056a865c52ab14151843b
Gabriel Palhares <34951844+gabriel-palhares@users.noreply.github.com>
2023-08-08 11:36:21 -0300

    Create population.c

---
+++ week-1/population.c
@@ -0,0 +1,30 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // TODO: Prompt for start size
+    int start;
+    do {
+        start = get_int("Start: ");
+    } while (start < 9);
+
+    // TODO: Prompt for end size
+    int end;
+    do {
+        end = get_int("End: ");
+    } while (end < start);
+
+    // TODO: Calculate number of years until we reach threshold
+    int n = start;
+    int years = 0;
+
+    while (n < end)
+    {
+        n = n + n/3 - n/4;
+        years++;
+    }
+
+    // TODO: Print number of years
+    printf("Years: %i\n", years);
+}

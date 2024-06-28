a7aa57d940f7ae29711af29cbdd480211398671b
Gabriel Palhares <34951844+minortypo@users.noreply.github.com>
2024-06-28 10:41:03 -0300

    Update helpers.py

--- week-9/pset9/helpers.py
+++ week-9/pset9/helpers.py
@@ -1,10 +1,11 @@
-import os
-import urllib.parse
+import csv
+import datetime
+import pytz
 import requests
+import urllib
+import uuid
 
-from flask import redirect, render_template, session
-from werkzeug.security import check_password_hash
-from datetime import datetime
+from flask import redirect, render_template, request, session
 from functools import wraps
 
 
@@ -17,7 +18,6 @@ def apology(message, code=400):
 
         https://github.com/jacebrowning/memegen#special-characters
         """
-
         for old, new in [
             ("-", "--"),
             (" ", "-"),
@@ -31,26 +31,20 @@ def apology(message, code=400):
             s = s.replace(old, new)
         return s
 
-    if isinstance(message, str):
-        message = escape(message)
-    else:
-        message = "Error occured!"
-
-    return render_template("apology.html", top=code, bottom=message), code
+    return render_template("apology.html", top=code, bottom=escape(message)), code
 
 
 def login_required(f):
     """
     Decorate routes to require login.
 
-    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
+    https://flask.palletsprojects.com/en/latest/patterns/viewdecorators/
     """
 
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if session.get("user_id") is None:
             return redirect("/login")
-
         return f(*args, **kwargs)
 
     return decorated_function
@@ -59,41 +53,36 @@ def login_required(f):
 def lookup(symbol):
     """Look up quote for symbol."""
 
-    # Contact API
-    try:
-        api_key = os.environ.get("API_KEY")
-        url = f"https://cloud.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}"
-        response = requests.get(url)
-        response.raise_for_status()
-    except requests.RequestException:
-        return None
+    # Prepare API request
+    symbol = symbol.upper()
+    end = datetime.datetime.now(pytz.timezone("US/Eastern"))
+    start = end - datetime.timedelta(days=7)
 
-    # Parse response
+    # Yahoo Finance API
+    url = (
+        f"https://query1.finance.yahoo.com/v7/finance/download/{urllib.parse.quote_plus(symbol)}"
+        f"?period1={int(start.timestamp())}"
+        f"&period2={int(end.timestamp())}"
+        f"&interval=1d&events=history&includeAdjustedClose=true"
+    )
+
+    # Query API
     try:
-        quote = response.json()
-        return {
-            "name": quote["companyName"],
-            "price": float(quote["latestPrice"]),
-            "symbol": quote["symbol"],
-        }
-    except (KeyError, TypeError, ValueError):
+        response = requests.get(
+            url,
+            cookies={"session": str(uuid.uuid4())},
+            headers={"Accept": "*/*", "User-Agent": request.headers.get("User-Agent")},
+        )
+        response.raise_for_status()
+
+        # CSV header: Date,Open,High,Low,Close,Adj Close,Volume
+        quotes = list(csv.DictReader(response.content.decode("utf-8").splitlines()))
+        price = round(float(quotes[-1]["Adj Close"]), 2)
+        return {"price": price, "symbol": symbol}
+    except (KeyError, IndexError, requests.RequestException, ValueError):
         return None
 
 
 def usd(value):
     """Format value as USD."""
-
     return f"${value:,.2f}"
-
-
-def get_time():
-    """Returns formatted local time."""
-
-    return datetime.now().strftime("%d-%m-%Y %H:%M:%S")
-
-
-def check_password(first_password, second_password):
-    """Checks the passwords to be a match. Returns a message error if they do not match."""
-
-    if not check_password_hash(first_password, second_password):
-        return apology("Passwords do not match!", code=401)
